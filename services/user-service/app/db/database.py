13cdcd89c888fbd8a69c61978b4971d75c458ff9
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-08-01 02:13:33 +0530

    Add user profile creation and retrieval endpoints; update database connection handling and requirements

--- services/user-service/app/db/database.py
+++ services/user-service/app/db/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://userservice:userservice@localhost:5433/userservice")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
