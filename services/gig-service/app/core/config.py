eeacdec779a43056a784b872e8dc5456682322cc
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-08-04 13:05:07 +0530

    Refactor config.py to define Settings class with environment variables

--- services/gig-service/app/core/config.py
+++ services/gig-service/app/core/config.py
@@ -1,1 +1,10 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    DATABASE_URL: str 
+    JWT_SECRET_KEY: str 
+    ALGORITHM: str 
+
+    class Config:
+        env_file = ".env"
+      
