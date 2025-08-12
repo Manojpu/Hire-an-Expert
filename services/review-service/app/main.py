237b85cd99f172391f54bc7fb6e8697e3795b582
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-08-12 20:31:38 +0530

    Set up FastAPI application with review endpoints and database initialization

--- services/review-service/app/main.py
+++ services/review-service/app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from app.endpoints.reviews import reviews
+from app.db.database import engine
+from app.models import review as review_model
+
+review_model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title = "Review Service")
 
+app.include_router(reviews.router, prefix="/api/reviews", tags=["reviews"])
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"service": "Review Service is running"}
+
 
 
